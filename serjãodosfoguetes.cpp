a811edfae0fdc51309521ac1c30082707f9c21a2
Giovanni dos Santos Tonsa <devsub0tonsa@gmail.com>
2025-09-30 11:43:50 -0300

    Add files via upload

---
+++ serjãodosfoguetes.cpp
@@ -0,0 +1,36 @@
+/*
+	Name: serjãodosfoguetes.cpp
+	Author: Giovanni Tonsa
+	Date: 30/09/25 11:10
+	Description: Programa que mostra a contagem regressiva de um foquete
+*/
+
+# include <stdio.h>
+# include <conio.h> 
+# include <windows.h>
+
+main()
+{
+	int lanca;
+	
+	while(1)
+	{
+		lanca = 0;
+		
+		
+		puts("Programa que mostra o lancamento de um foguete");
+		puts("==============================================");
+		printf("Digite seu tempo de lancamento: ");
+		scanf("%d", &lanca);
+		
+		for (lanca = lanca; lanca >= 0; lanca--)
+		{
+			printf("\nContagem: %d", lanca);
+			Sleep(1000);
+		}
+		puts("\nFogo!!!");
+	system("pause");
+	system("cls");
+	}//fim do while
+}//fim do programa
+
